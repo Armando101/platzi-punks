66c0163fdddfe6262d4c3ab3c705771d7d946e9f
Armando Rivera <rivera.armando997@gmail.com>
2021-12-22 14:21:59 -0600

    smart-contract - extend functionality

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const deploy = async () => {
+  const [depoyer] = await ethers.getSigners();
+
+  console.log("deploying contract with the account", depoyer.address);
+  const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+  const deployed = await PlatziPunks.deploy();
+
+  console.log("Platzi Punks deployed at", deployed.address);
+};
+
+deploy()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
